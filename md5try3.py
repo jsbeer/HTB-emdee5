52a563b90d6a95c5de6c3fe5c96905c162c4946b
Jonathan Strand-Beer <46929246+jsbeer@users.noreply.github.com>
2020-03-11 07:39:45 -0400

    Add files via upload

---
+++ md5try3.py
@@ -0,0 +1,45 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import webbrowser
+import urllib.request
+import requests
+import nltk
+import re
+import hashlib
+
+
+
+
+
+chromedriver_location = "/usr/bin/chromedriver"
+
+
+
+
+driver = webdriver.Chrome(chromedriver_location)
+driver.get('http://docker.hackthebox.eu:31332/')
+
+
+
+first = '/html/body/center/form/input[1]'
+
+second = '/html/body/center/form/input[2]'
+
+third = '/html/body/h3'
+
+
+PP = driver.find_element_by_xpath(third).text
+LL = hashlib.md5(PP.encode())
+LL = LL.hexdigest()
+
+driver.find_element_by_xpath(first).send_keys(LL)
+
+
+driver.find_element_by_xpath(second).click()
+
+
+print (PP)
+
+print(LL)
